23d59c1a7f77b4fbe78888bab8c0c10e962da790
A Ramanuj Patro <ramanujpatro07@gmail.com>
2026-02-03 03:19:33 +0530

    stock

--- src/vite-env.d.ts
+++ src/vite-env.d.ts
@@ -1,1 +1,10 @@
 /// <reference types="vite/client" />
+
+interface ImportMetaEnv {
+    readonly VITE_FINNHUB_API_KEY: string;
+    // Add more env variables as needed
+}
+
+interface ImportMeta {
+    readonly env: ImportMetaEnv;
+}
